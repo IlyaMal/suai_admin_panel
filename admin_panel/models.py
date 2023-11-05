516cbbd86efa92840b216a248ed231296fd6c838
sanfit <sanfit@vk.com>
2023-11-05 16:05:35 +0300

    update

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -2,25 +2,28 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=255)
+    username = models.CharField(verbose_name="Имя пользователя", max_length=255)
 
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(verbose_name="Имя", max_length=255)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=255)
 
-    group_number = models.IntegerField()
-    ticket_number = models.CharField(max_length=255)
-    telegram_id = models.BigIntegerField()
-    current_query_id = models.BigIntegerField()
+    group_number = models.IntegerField(verbose_name="Номер группы")
+    ticket_number = models.CharField(verbose_name="Номер профкома", max_length=255)
+    telegram_id = models.BigIntegerField(verbose_name="Телеграмм ID")
+    current_query_id = models.BigIntegerField(verbose_name="Текущий номер запроса")
 
     class Meta:
         verbose_name = 'Студент'
         verbose_name_plural = 'Студенты'
 
+    def __str__(self) -> str:
+        return self.username
+
 
 
 class Query(models.Model):
-    text = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)
+    text = models.CharField(verbose_name="Текст", max_length=255)
+    type = models.CharField(verbose_name="Тип", max_length=255)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
